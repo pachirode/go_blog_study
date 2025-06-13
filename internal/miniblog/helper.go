fe0a00ac85af0eedebab0affe24e581704d259c9
pachirode <pachirode@gmail.com>
2025-06-13 22:06:46 +0800

    6/13

---
+++ internal/miniblog/helper.go
@@ -0,0 +1,47 @@
+package miniblog
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	recommendedHomeDir = ".miniblog"
+
+	defaultConfigName = "miniblog.yaml"
+)
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(filepath.Join(home, recommendedHomeDir))
+
+		viper.AddConfigPath(".")
+
+		viper.SetConfigType("yaml")
+
+		viper.SetConfigName(defaultConfigName)
+	}
+
+	viper.AutomaticEnv()
+
+	viper.SetEnvPrefix("MINIBLOG")
+
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Viper parsing error: ", err)
+	}
+
+	fmt.Println("Using config file:", viper.ConfigFileUsed())
+}
--- internal/miniblog/miniblog.go
+++ internal/miniblog/miniblog.go
@@ -1,27 +1,40 @@
 package miniblog
+
 import (
-    "fmt"
-    "github.com/spf13/cobra"
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
+
 func NewMiniBlogCommand() *cobra.Command {
-    cmd := &cobra.Command{
-        Use: "miniblog",
-        SilenceUsage: true,
-        RunE: func(cmd *cobra.Command, args []string) error {
-            return run()
-        },
-        Args: func(cmd *cobra.Command, args []string) error {
-            for _, arg := range args {
-                if len(arg) > 0 {
-                    return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
-                }
-            }
-            return nil
-        },
-    }
-    return cmd
+	cmd := &cobra.Command{
+		Use:          "miniblog",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run()
+		},
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+			return nil
+		},
+	}
+	cobra.OnInitialize(initConfig)
+
+	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the miniblog config file")
+
+	return cmd
 }
+
 func run() error {
-    fmt.Println("Hello MiniBlog!")
-    return nil
+	setting, _ := json.Marshal(viper.AllSettings())
+	fmt.Println("Config file settings: ", string(setting))
+	return nil
 }
