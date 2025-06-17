e7b43ebea76822e939f69fdfcc64cf19160f2b69
pachirode <pachirode@gmail.com>
2025-06-17 22:17:34 +0800

    增加 web 服务 demo

--- internal/miniblog/miniblog.go
+++ internal/miniblog/miniblog.go
@@ -2,8 +2,11 @@ package miniblog
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/marmotedu/Miniblog/internal/pkg/log"
 	"github.com/marmotedu/Miniblog/pkg/version/verflag"
 	"github.com/spf13/cobra"
@@ -42,6 +45,24 @@ func NewMiniBlogCommand() *cobra.Command {
 
 func run() error {
 	setting, _ := json.Marshal(viper.AllSettings())
-	log.Infow("Config settings: ", string(setting))
+	log.Debugw("Config settings: ", string(setting))
+
+	gin.SetMode(viper.GetString("web.runmode"))
+	g := gin.New()
+
+	g.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"code": 500, "msg": "Page not found"})
+	})
+
+	g.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"code": 200})
+	})
+
+	httpServe := http.Server{Addr: viper.GetString("web.addr"), Handler: g}
+	log.Infow("Starting listen requests on http address", "addr", viper.GetString("web.addr"))
+
+	if err := httpServe.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalw(err.Error())
+	}
 	return nil
 }
